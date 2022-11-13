aa39cde28d10d5fcc43f622afc6c31f3a5439289
Pipin Yulianto <201210068@student.mercubuana-yogya.ac.id>
2022-11-13 17:01:56 +0700

    Belajar Search Movie Master Continue

---
+++ app/src/main/java/com/chirikualii/materidb/ui/search/SearchActivity.kt
@@ -0,0 +1,60 @@
+package com.chirikualii.materidb.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ExpandableListAdapter
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
+import com.chirikualii.materidb.R
+import com.chirikualii.materidb.data.model.Movie
+import com.chirikualii.materidb.databinding.ActivitySearchBinding
+import com.chirikualii.materidb.ui.adapter.MovieListAdapter
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+    private lateinit var listAdapter: MovieListAdapter
+
+
+    val viewModel :SearchViewModel by viewModels<SearchViewModel>(
+        factoryProducer = { SearchViewModelFactroy(context = this)}
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        observeView()
+
+
+        listAdapter = MovieListAdapter()
+        binding.rvMovie.adapter = listAdapter
+
+
+
+        binding.etSearch.doOnTextChanged { text, _, _, _ ->
+            viewModel.doSearchMovie(text.toString())
+            Toast.makeText(this, "searching ${text}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun observeView() {
+        viewModel.listMovie.observe(this){
+            listAdapter.addItem(it)
+        }
+
+        viewModel.isLoading.observe(this){
+            if (it){
+                binding.progressBar.visibility = View.VISIBLE
+            }else{
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+}
